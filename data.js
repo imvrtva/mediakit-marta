8c3814c12533fb48ea84d2f26bb5f8a067c9dc09
Marta Cazzin <martadinome@gmail.com>
2025-08-20 14:51:57 +0200

    modifiche

--- data.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const data = {
-    instagramFollowers: "12.3K",
-    tiktokFollowers: "45K"
-  };
-
-  document.getElementById("ig-followers").textContent = data.instagramFollowers;
-  document.getElementById("tt-followers").textContent = data.tiktokFollowers;
-});
